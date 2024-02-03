1419fc1868e854fd754ce527fe5e71f9bc679de5
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-02-03 23:24:43 +0200

    worked on routing using node and express routing

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { PORT, MONGO_URL } from "./config.js"; //the file system has to be explicitly stated with ending ".js", otherwise it wont work
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
 const app = express();
+
+//middleware for parsing request body
+//ensuring that your Express application can handle incoming JSON data from client requests, which you can then use as needed, including passing it to Mongoose for database operations if that's part of your application logic.
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to the channel");
 });
+
+app.use('/books', booksRoute);
 
 mongoose
   .connect(MONGO_URL)
